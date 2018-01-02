32ffd4c574e6d007bbd72b7628e0b15ced77d294
zhenghengZN <zhengheng@Mac-mini.local>
2018-01-02 16:49:27 +0800

    test

--- app/src/main/java/so/bubu/ui/test/myapplication/MainActivity.java
+++ app/src/main/java/so/bubu/ui/test/myapplication/MainActivity.java
@@ -3,28 +3,36 @@ package so.bubu.ui.test.myapplication;
 
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import so.bubu.ui.test.mylibrary.page.common.BaseActivity;
 
 public class MainActivity extends BaseActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        setBackClick();
-    }
-
-    @Override
-    protected void onCreateView(Bundle savedInstanceState) {
-
-    }
 
     @Override
     protected void doBack(int keyCode, KeyEvent event) {
         finish();
     }
 
+    @Override
+    public View addBaseContenetView() {
+        return LayoutInflater.from(this).inflate(R.layout.activity_main, null);
+    }
+
+    @Override
+    public void doInCreateView() {
+        Button btn = (Button) findViewById(R.id.test);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"test",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 }
--- mylibrary/src/main/java/so/bubu/ui/test/mylibrary/page/common/BaseActivity.java
+++ mylibrary/src/main/java/so/bubu/ui/test/mylibrary/page/common/BaseActivity.java
@@ -1,15 +1,37 @@
 package so.bubu.ui.test.mylibrary.page.common;
 
 import android.os.Bundle;
+import android.support.annotation.LayoutRes;
 import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.LinearLayout;
 
 import so.bubu.ui.test.mylibrary.R;
 
 /**
  * Created by zhengheng on 18/1/2.
  */
-public abstract class BaseActivity extends BaseCompatActivity{
+public abstract class BaseActivity extends BaseCompatActivity {
+
+    private LinearLayout view;
+
+    @Override
+    protected void onCreateView(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_base);
+        view = (LinearLayout) findViewById(R.id.base_scroll_content);
+        view.addView(addBaseContenetView());
+        doInCreateView();
+    }
+
+//    public View addBaseContenetView(int layoutResID) {
+//      return  LayoutInflater.from(this).inflate(layoutResID, null);
+//    }
+
+    public abstract View addBaseContenetView();
+
+
+    public abstract void doInCreateView();
 
     /**
      * 设置后退按钮
