16fdb4b724eeaa6233f688799623b1ba2a5b83e8
zhenghengZN <zhengheng@Mac-mini.local>
2018-01-03 09:40:57 +0800

    test

--- mylibrary/src/main/java/so/bubu/ui/test/mylibrary/Adapter/CouponsItemAdapter.java
+++ mylibrary/src/main/java/so/bubu/ui/test/mylibrary/Adapter/CouponsItemAdapter.java
@@ -31,22 +31,21 @@ public abstract class CouponsItemAdapter<T extends CouponsItemBean.ObjectsBean> 
     @Override
     public void convert(RecyclerViewHolder holder, Object item, int position) {
 
-//        CouponItemBean.ObjectsBean taobaoContentBean = (T) mDatasList.get(position);
-//        T taobaoContentBean = (T) mDatasList.get(position);
-//        holder.setText(R.id.taobao_pro_desc, taobaoContentBean.getTitle());
-//        holder.setText(R.id.taobao_finalPrice, taobaoContentBean.getFinalPrice() + " ");
-//
-//        holder.setText(R.id.biz30Day, "月销:" + taobaoContentBean.getBiz30Day());
-//        holder.setText(R.id.couponAmount, "立减 " + taobaoContentBean.getCouponAmount() + " 元");
-//
-//        if (taobaoContentBean.getPlatform().equals("天猫")) {
-//            holder.setImageResource(R.id.taobao_platform, R.drawable.tmall_logo_30);
-//            holder.setText(R.id.taobao_discountPrice, "天猫价 ¥" + taobaoContentBean.getDiscountPrice());
-//        } else {
-//            holder.setImageResource(R.id.taobao_platform, R.drawable.taobao_logo_30);
-//            holder.setText(R.id.taobao_discountPrice, "淘宝价 ¥" + taobaoContentBean.getDiscountPrice());
-//        }
-//        CouponsItemAdapter.displayImageByResizeasBitmap(mContext, CommonMethod.getThumbUrl(taobaoContentBean.getPicUrl(), ResourceUtil.Dp2Px(115), ResourceUtil.Dp2Px(115)), ResourceUtil.Dp2Px(115), ResourceUtil.Dp2Px(115), (ImageView) holder.getView(R.id.product_img));
+        T taobaoContentBean = (T) mDatasList.get(position);
+        holder.setText(R.id.taobao_pro_desc, taobaoContentBean.getTitle());
+        holder.setText(R.id.taobao_finalPrice, taobaoContentBean.getFinalPrice() + " ");
+
+        holder.setText(R.id.biz30Day, "月销:" + taobaoContentBean.getBiz30Day());
+        holder.setText(R.id.couponAmount, "立减 " + taobaoContentBean.getCouponAmount() + " 元");
+
+        if (taobaoContentBean.getPlatform().equals("天猫")) {
+            holder.setImageResource(R.id.taobao_platform, R.drawable.tmall_logo_30);
+            holder.setText(R.id.taobao_discountPrice, "天猫价 ¥" + taobaoContentBean.getDiscountPrice());
+        } else {
+            holder.setImageResource(R.id.taobao_platform, R.drawable.taobao_logo_30);
+            holder.setText(R.id.taobao_discountPrice, "淘宝价 ¥" + taobaoContentBean.getDiscountPrice());
+        }
+        CouponsItemAdapter.displayImageByResizeasBitmap(mContext, CommonMethod.getThumbUrl(taobaoContentBean.getPicUrl(), ResourceUtil.Dp2Px(115), ResourceUtil.Dp2Px(115)), ResourceUtil.Dp2Px(115), ResourceUtil.Dp2Px(115), (ImageView) holder.getView(R.id.product_img));
         doOther(holder, mDatasList.get(position), position);
         holder.itemView.setTag(position);
     }
